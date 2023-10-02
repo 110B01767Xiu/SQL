49ba6fdb7a862f51de01b4e01be5698bf5fa095b
110B01767Xiu <115057022+110B01767Xiu@users.noreply.github.com>
2023-10-02 12:11:29 +0800

    Update 3-2-6ex01.sql

--- Lab03/3-2-6ex01.sql
+++ Lab03/3-2-6ex01.sql
@@ -1,6 +1,6 @@
-INSERT INTO Employee
-VALUES('S0006', '六合', 'D010')
-
+會產生如下錯誤訊息:
 訊息 547，層級 16，狀態 0，行 1
 INSERT 陳述式與 FOREIGN KEY 條件約束 "FK__Employee__Em_cod__398D8EEE" 衝突。衝突發生在資料庫 "Company"，資料表 "dbo.Comp_dep", column 'Comp_dep_code'。
 陳述式已經結束。
+
+因為子關聯表的部碼(外鍵)無法參考到父關聯表的部碼(主鍵)
