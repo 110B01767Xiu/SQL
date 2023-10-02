00290d74e32ccc2a81cb2fb03dd16cd404aba9ed
110B01767Xiu <115057022+110B01767Xiu@users.noreply.github.com>
2023-10-02 09:17:43 +0800

    Create 3-2-6ex01.sql

---
+++ Lab03/3-2-6ex01.sql
@@ -0,0 +1,6 @@
+INSERT INTO Employee
+VALUES('S0006', '六合', 'D010')
+
+訊息 547，層級 16，狀態 0，行 1
+INSERT 陳述式與 FOREIGN KEY 條件約束 "FK__Employee__Em_cod__398D8EEE" 衝突。衝突發生在資料庫 "Company"，資料表 "dbo.Comp_dep", column 'Comp_dep_code'。
+陳述式已經結束。
